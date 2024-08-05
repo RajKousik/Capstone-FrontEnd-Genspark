b4a8c1a837869bb356c180c21c5f5d1f5daff861
Kousik <rajkousik20@gmail.com>
2024-08-05 09:06:01 +0530

    updated img src

--- src/api/utility/genreTypes.js
+++ src/api/utility/genreTypes.js
@@ -3,61 +3,61 @@ const GenreTypes = {
     id: 1,
     name: "Pop",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346837/pop_dcy0qm.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/pop_cin2sb.jpg",
   },
   ROCK: {
     id: 2,
     name: "Rock",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346839/rock_iyotrj.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/rock_cmlpga.jpg",
   },
   JAZZ: {
     id: 3,
     name: "Jazz",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346839/jazz_id8tjo.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/jazz_kxjqey.jpg",
   },
   CLASSICAL: {
     id: 4,
     name: "Classical",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346840/classical_dzvcrt.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/classical_gbankk.jpg",
   },
   HIP_HOP: {
     id: 5,
     name: "HipHop",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346841/hiphop_kisw5p.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/hiphop_cobvtr.jpg",
   },
   ELECTRONIC: {
     id: 6,
     name: "Electronic",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346842/electronic_kfjsar.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/electronic_vm0j56.jpg",
   },
   COUNTRY: {
     id: 7,
     name: "Country",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346843/country_iva0ie.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/country_xojvst.jpg",
   },
   BLUES: {
     id: 8,
     name: "Blues",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346837/blues_tpgh9u.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828773/blues_ihunh9.jpg",
   },
   REGGAE: {
     id: 9,
     name: "Reggae",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346837/raggae_u2ujse.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828773/raggae_uoxu5s.jpg",
   },
   METAL: {
     id: 10,
     name: "Metal",
     imageUrl:
-      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722346837/metal_cb66mf.jpg",
+      "https://res.cloudinary.com/deqk5oxse/image/upload/v1722828774/metal_hv5pd5.jpg",
   },
 };
 
